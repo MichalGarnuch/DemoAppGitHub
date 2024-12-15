74446e246510218f51f20099f55c4865fdfbf702
Michal <m.p.garnuch@gmail.com>
2024-12-15 16:31:57 +0100

    Add project files.

---
+++ DemoAppGitHub/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DemoAppGitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, GitHub!"); // Prosty komunikat
+            Console.WriteLine("Dzisiaj jest: " + DateTime.Now); // Aktualna data
+            Console.ReadLine(); // Zatrzymanie konsoli
+        }
+    }
+}
