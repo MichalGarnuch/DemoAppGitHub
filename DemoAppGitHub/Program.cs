fcedbb8ffbd262cfdadedaba69037d92905856c8
Michal <m.p.garnuch@gmail.com>
2024-12-15 19:13:59 +0100

    Inne dodatkwoe wyswietlanie

--- DemoAppGitHub/Program.cs
+++ DemoAppGitHub/Program.cs
@@ -7,6 +7,9 @@ namespace DemoAppGitHub
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, GitHub!"); // Prosty komunikat
+            Console.WriteLine("Witaj ponownie, GitHub!");
+            Console.WriteLine("Witaj 1ponownie, GitHub!");
+            Console.WriteLine("Witaj 2ponownie, GitHub!");
             Console.WriteLine("Dzisiaj jest: " + DateTime.Now); // Aktualna data
             Console.WriteLine("Witaj ponownie, GitHub!");
             Console.ReadLine(); // Zatrzymanie konsoli
