9fdaa097f7f93f32dfab00dfe68fddd31a01adae
Michal <m.p.garnuch@gmail.com>
2024-12-15 20:28:37 +0100

    kluczowe modyfikacje

--- DemoAppGitHub/Program.cs
+++ DemoAppGitHub/Program.cs
@@ -6,12 +6,12 @@ namespace DemoAppGitHub
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("a tego nigdy nie było!");
             Console.WriteLine("Hello, GitHub!"); // Prosty komunikat
             Console.WriteLine("Witaj ponownie, GitHub!");
-            Console.WriteLine("Witaj 1ponownie, GitHub!");
-            Console.WriteLine("Witaj 2ponownie, GitHub!");
             Console.WriteLine("Dzisiaj jest: " + DateTime.Now); // Aktualna data
-            Console.WriteLine("Witaj ponownie, GitHub!");
+            Console.WriteLine("tak tam czesc usunałem tu dodałem!");
             Console.ReadLine(); // Zatrzymanie konsoli
 
         }
